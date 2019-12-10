76d6ebd0edf7c3090134205d3fd6e156c6497b18
Michel Gracia <michel.gds@hotmail.com>
2019-12-10 18:08:01 -0300

    Update amount on cart itens

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -11,16 +11,25 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, removeFromCart, updateAmount }) {
+  function increment(product) {
+    updateAmount(product.id, product.amount + 1);
+  }
+
+  function decrement(product) {
+    updateAmount(product.id, product.amount - 1);
+  }
   return (
     <Container>
       <ProductTable>
         <thead>
-          <th />
-          <th>PRODUTO</th>
-          <th>QTD</th>
-          <th>SUBTOTAL</th>
-          <th />
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
         </thead>
         <tbody>
           {cart.map(product => (
@@ -34,11 +43,11 @@ function Cart({ cart, removeFromCart }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button type="button" onClick={() => decrement(product)}>
                     <MdRemoveCircleOutline size={20} color="7159c1" />
                   </button>
                   <input type="number" readOnly value={product.amount} />
-                  <button type="button">
+                  <button type="button" onClick={() => increment(product)}>
                     <MdAddCircleOutline size={20} color="7159c1" />
                   </button>
                 </div>
